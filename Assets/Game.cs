dccf871ac27616388030f2528a6e74dc51c83b32
Sabaniki <sawada13@mccc.jp>
2020-05-11 22:47:11 +0900

    課題3まで完成

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -1,6 +1,12 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Sequence = System.Collections.IEnumerator;
+using JetBrains.Annotations;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+//using Random = System.Random;
 
 /// <summary>
 /// ゲームクラス。
@@ -21,10 +27,7 @@ public sealed class Game : GameBase {
     private void ResetValues() {
         // キャンバスの大きさを設定します
         gc.SetResolution(720, 1280);
-        cardGenerator = new CardGenerator(
-            new List<string> {
-                "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"
-            });
+        cardGenerator = new CardGenerator();
         cardName = "";
     }
 
@@ -34,10 +37,10 @@ public sealed class Game : GameBase {
     /// </summary>
     public override void UpdateGame() {
         // 起動からの経過時間を取得します
-        if (gc.GetPointerFrameCount(0) == 1 && !cardGenerator.IsComplete) {
-            cardName = cardGenerator.DrawCard();
+        if ( /*gc.GetPointerFrameCount(0) == 1 && */!cardGenerator.IsComplete) {
+            cardName = cardGenerator.DrawCard() ?? "no card";
         }
-        
+
         if (gc.GetPointerFrameCount(0) >= 120) {
             ResetValues();
         }
@@ -54,6 +57,8 @@ public sealed class Game : GameBase {
         gc.DrawString($"money: {cardGenerator.Money}", 60, 40);
         gc.DrawString($"new: {cardName}", 60, 80);
 
+        gc.Random(0, 10);
+
         foreach (var (item, index) in cardGenerator.CardHistory.Select((item, index) => (item, index)))
             gc.DrawString($"{item.Key}: {item.Value.ToString()}", 60, 120 + index * 40);
 
@@ -64,27 +69,48 @@ public sealed class Game : GameBase {
 
 
 public class CardGenerator {
-    private readonly List<string> cardNames;
+    private readonly Dictionary<string, float> cardNamesAndRarity;
+    public readonly List<string> cardNames;
     public int Money { private set; get; }
     public bool IsComplete { private set; get; }
-    public readonly IDictionary<string, int> CardHistory;
+    public readonly Dictionary<string, int> CardHistory;
 
-    public CardGenerator(List<string> cardNames, int initMoney = 10000) {
-        this.cardNames = cardNames;
+    public CardGenerator(int initMoney = 10000) {
+        cardNamesAndRarity = new Dictionary<string, float> {
+            {"A", 0.05f}, {"B", 0.05f}, {"C", 0.05f}, {"D", 0.05f}, {"E", 0.05f},
+            {"F", 0.15f}, {"G", 0.15f}, {"H", 0.15f}, {"I", 0.15f}, {"J", 0.15f}
+        };
+        cardNames = new List<string>();
         IsComplete = false;
         Money = initMoney;
         CardHistory = new Dictionary<string, int>();
-        foreach (var cardName in cardNames)
+        foreach (var cardName in cardNamesAndRarity.Keys) {
+            cardNames.Add(cardName);
             CardHistory.Add(cardName, 0);
+        }
+    }
+    
+    [CanBeNull]
+    private string Choose(){
+        var total = cardNamesAndRarity.Sum(elem => elem.Value);
+        var randomPoint = Random.value * total;
+        foreach (var elem in cardNamesAndRarity) {
+            if (!(randomPoint < elem.Value))
+                randomPoint -= elem.Value;
+            else
+                return elem.Key;
+        }
+        return null;
     }
 
+    [CanBeNull]
     public string DrawCard() {
-        Money -= 100;
         if (Money <= 0) return null;
-        var random = new System.Random().Next(0, cardNames.Count);
-        var cardName = cardNames[random];
+        Money -= 100;
+        var cardName = Choose();
+        if (cardName == null) return null;
         CardHistory[cardName]++;
         IsComplete = CardHistory.Values.All(count => count > 0);
-        return cardNames[random];
+        return cardName;
     }
 }
